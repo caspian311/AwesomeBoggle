b4da39cb803f0fec48ae5947645db6513ff3f451
Matt Todd <matt.c.todd@gmail.com>
2017-02-07 17:32:34 -0600

    made current word label and submit button

--- AwesomeBoggle/BoggleView.swift
+++ AwesomeBoggle/BoggleView.swift
@@ -14,40 +14,81 @@ class BoggleView: UIView {
         self.gridRows = UIStackView()
         
         super.init(frame: CGRect.zero)
-        self.backgroundColor = .blue
+        self.backgroundColor = .gray
         
         let mainVerticalStack = UIStackView()
         mainVerticalStack.axis = .vertical
+        mainVerticalStack.backgroundColor = .gray
+        
+        let resultsStack = UIStackView()
+        resultsStack.axis = .horizontal
+        resultsStack.backgroundColor = .gray
         
         addSubview(mainVerticalStack)
         
         let resetButton = UIButton()
-        resetButton.backgroundColor = .white
-        resetButton.setTitle("Reset", for: .normal)
-        resetButton.setTitleColor(.black, for: .normal)
         
         mainVerticalStack.addArrangedSubview(resetButton)
         mainVerticalStack.addArrangedSubview(self.gridRows)
+        mainVerticalStack.addArrangedSubview(resultsStack)
         
         mainVerticalStack.translatesAutoresizingMaskIntoConstraints = false
         mainVerticalStack.topAnchor.constraint(equalTo: self.topAnchor, constant: 20).isActive = true
         mainVerticalStack.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
         mainVerticalStack.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
         
+        resetButton.backgroundColor = .white
+        resetButton.layer.borderColor = UIColor.red.cgColor
+        resetButton.layer.borderWidth = 1
+        resetButton.layer.cornerRadius = 10
+        
+        resetButton.setTitle("Reset", for: .normal)
+        resetButton.setTitleColor(.black, for: .normal)
+        
         resetButton.translatesAutoresizingMaskIntoConstraints = false
         resetButton.topAnchor.constraint(equalTo: mainVerticalStack.topAnchor, constant: 20).isActive = true
-        resetButton.leadingAnchor.constraint(equalTo: mainVerticalStack.leadingAnchor).isActive = true
-        resetButton.widthAnchor.constraint(equalTo: mainVerticalStack.widthAnchor).isActive = true
+        resetButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
         resetButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
         
         self.gridRows.axis = .vertical
         self.gridRows.translatesAutoresizingMaskIntoConstraints = false
         self.gridRows.leadingAnchor.constraint(equalTo: mainVerticalStack.leadingAnchor).isActive = true
-        self.gridRows.topAnchor.constraint(equalTo: resetButton.bottomAnchor, constant: 20).isActive = true
+        self.gridRows.topAnchor.constraint(equalTo: resetButton.bottomAnchor, constant: 30).isActive = true
         self.gridRows.widthAnchor.constraint(equalTo: mainVerticalStack.widthAnchor).isActive = true
         self.gridRows.backgroundColor = .white
         
         resetButton.addTarget(self, action: #selector(resetButtonTapped), for: .touchUpInside)
+        
+        let currentWorldLabel = UILabel()
+        resultsStack.addArrangedSubview(currentWorldLabel)
+        
+        currentWorldLabel.backgroundColor = .white
+        currentWorldLabel.layer.masksToBounds = true
+        currentWorldLabel.layer.borderColor = UIColor.red.cgColor
+        currentWorldLabel.layer.borderWidth = 1
+        currentWorldLabel.layer.cornerRadius = 10
+        
+        currentWorldLabel.translatesAutoresizingMaskIntoConstraints = false
+        currentWorldLabel.topAnchor.constraint(equalTo: self.gridRows.bottomAnchor, constant: 20).isActive = true
+        currentWorldLabel.leadingAnchor.constraint(equalTo: mainVerticalStack.leadingAnchor).isActive = true
+        currentWorldLabel.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        
+        let submitWordButton = UIButton()
+        resultsStack.addArrangedSubview(submitWordButton)
+        
+        submitWordButton.setTitle("Enter", for: .normal)
+        submitWordButton.setTitleColor(.black, for: .normal)
+        
+        submitWordButton.backgroundColor = .white
+        submitWordButton.layer.borderColor = UIColor.red.cgColor
+        submitWordButton.layer.borderWidth = 1
+        submitWordButton.layer.cornerRadius = 10
+        
+        submitWordButton.translatesAutoresizingMaskIntoConstraints = false
+        submitWordButton.topAnchor.constraint(equalTo: self.gridRows.bottomAnchor, constant: 20).isActive = true
+        submitWordButton.leadingAnchor.constraint(equalTo: currentWorldLabel.trailingAnchor, constant: 10).isActive = true
+        submitWordButton.heightAnchor.constraint(equalTo: currentWorldLabel.heightAnchor).isActive = true
+        submitWordButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
     }
     
     func clearGrid() {
@@ -71,14 +112,17 @@ class BoggleView: UIView {
                 buttonToAdd.widthAnchor.constraint(equalTo: stackView.widthAnchor, multiplier: 1/4).isActive = true
                 buttonToAdd.heightAnchor.constraint(equalTo: buttonToAdd.widthAnchor).isActive = true
                 
-                buttonToAdd.backgroundColor = colors[colorCounter]
+                buttonToAdd.backgroundColor = .white
+                buttonToAdd.layer.borderColor = UIColor.red.cgColor
+                buttonToAdd.layer.borderWidth = 1
+                buttonToAdd.layer.cornerRadius = 10
                 
                 
                 buttonToAdd.setTitle(letters[colorCounter], for: .normal)
                 
                 colorCounter += 1
                 
-                buttonToAdd.setTitleColor(.purple, for: .normal)
+                buttonToAdd.setTitleColor(.black, for: .normal)
                 
                 buttonToAdd.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
             }
